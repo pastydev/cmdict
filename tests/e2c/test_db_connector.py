04297ebadf5741f404b95b708f43fe962c9c0039
edxu96 <edxu96@outlook.com>
2020-07-04 20:30:54 +0200

    remove tests for search function

--- tests/e2c/test_db_connector.py
+++ tests/e2c/test_db_connector.py
@@ -1,8 +1,7 @@
 """Test DBConnector."""
-from click.testing import CliRunner
 import pytest
 
-from e2c.db_connector import DBConnector, search
+from e2c.db_connector import DBConnector
 
 
 def test_db_connect():
@@ -15,9 +14,3 @@ def test_no_file_connect():
     """Test database connection when .db file doesn't exist."""
     with pytest.raises(ValueError):
         DBConnector("./no_such_file").search("play")
-
-
-def test_search():
-    """Test search function in command line."""
-    res = CliRunner().invoke(search, "play")
-    assert not res.exception
