6b9518e1dcf4724d35c9c02b9f77c7e0a32666a5
edxu96 <edxu96@outlook.com>
2020-07-04 18:44:33 +0200

    add a test for search in command line

--- tests/e2c/test_db_connector.py
+++ tests/e2c/test_db_connector.py
@@ -1,16 +1,23 @@
 """Test DBConnector."""
+from click.testing import CliRunner
 import pytest
 
-from e2c.db_connector import DBConnector
+from e2c.db_connector import DBConnector, search
 
 
 def test_db_connect():
-    """Test database connection."""
-    res = DBConnector().search("play")
-    assert isinstance(res, tuple) and len(res) > 0
+    """Test database connection and single query."""
+    res = DBConnector().query("play")
+    assert isinstance(res, dict) and len(res) > 0
 
 
 def test_no_file_connect():
     """Test database connection when .db file doesn't exist."""
     with pytest.raises(ValueError):
         DBConnector("./no_such_file").search("play")
+
+
+def test_search():
+    """Test search function in command line."""
+    res = CliRunner().invoke(search, "play")
+    assert not res.exception
