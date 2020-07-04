7cca87cd5c117de68827af58d535f2c3d43ad50b
edxu96 <edxu96@outlook.com>
2020-07-04 20:32:49 +0200

    remove search function

--- src/e2c/db_connector.py
+++ src/e2c/db_connector.py
@@ -3,20 +3,35 @@ import os
 import pathlib
 import sqlite3
 
-import click
 from loguru import logger
 
 _path = os.path.join(os.getcwd(), "db", "stardict.db")
+_key_names = (
+    "id",
+    "word",
+    "sw",
+    "phonetic",
+    "definition",
+    "trans",
+    "pos",
+    "collins",
+    "oxford",
+    "tag",
+    "bnc",
+    "frq",
+    "exchange",
+)
 
 
 class DBConnector:
     """Database Connector."""
 
-    def __init__(self, path: str = _path):
+    def __init__(self, path=_path):
         """Initialize database Connector.
 
         Args:
-            path (str, optional): Path to the database file. Defaults to _path.
+            path (str, optional): Path to the database file.
+                Defaults to ``_path``.
         """
         if pathlib.Path(path).is_file() and path.endswith(".db"):
             self._conn = DBConnector._init_conn(path)
@@ -24,47 +39,20 @@ class DBConnector:
             raise ValueError("Database file is missing or invalid.")
 
     @staticmethod
-    def _init_conn(path: str) -> sqlite3.Connection:
+    def _init_conn(path):
         """Initialize database connection."""
         try:
             return sqlite3.connect(path)
         except sqlite3.Error:
             logger.exception("SQLite DB connection failed.")
 
-    def query(self, word: str = "wrong") -> dict:
+    def query(self, word="wrong"):
         """Search word from the database."""
         try:
             query = "select * from stardict where word = ?"
             cursor = self._conn.cursor()
             cursor.execute(query, (word,))
             res = cursor.fetchone()
-            res_dict = {
-                "id": res[0],
-                "word": res[1],
-                "sw": res[2],
-                "phonetic": res[3],
-                "definition": res[4],
-                "trans": res[5],
-                "pos": res[6],
-                "collins": res[7],
-                "oxford": res[8],
-                "tag": res[9],
-                "bnc": res[10],
-                "frq": res[11],
-                "exchange": res[12],
-            }
-            return res_dict
+            return dict([(x, y) for x, y in zip(_key_names, res)])
         except sqlite3.Error:
             logger.exception("SQLite DB search failed.")
-
-
-@click.command()
-@click.argument("word")
-def search(word):
-    """Type in English words and return Chinese translations."""
-    res = DBConnector().query(word)
-    click.echo("\n" + res["definition"] + "\n\n" + res["trans"])
-
-
-if __name__ == "__main__":
-    search()
