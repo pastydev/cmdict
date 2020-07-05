898cac2b7cfd5531949e8d9f61e19cc60f063a98
Zequn Yu <zequnyu11@gmail.com>
2020-07-05 13:23:43 -0700

    Add script to invoke cli directly (#20)

--- tests/e2c/test_run_script.py
+++ tests/e2c/test_run_script.py
@@ -1,10 +1,16 @@
 """Test functions for seaching in command line."""
 from click.testing import CliRunner
 
-from e2c.search_func import search
+from e2c.run_script import cli, search
 
 
-def test_search():
+def test_cli():
+    """Test cli not crash."""
+    res = CliRunner().invoke(cli)
+    assert not res.exception
+
+
+def test_cli_search():
     """Test search function in command line."""
     res = CliRunner().invoke(search, "play")
     assert not res.exception
