61c2cc3fa6af270a814c00f3fdd613362c242f54
Zequn Yu <zequnyu11@gmail.com>
2020-07-08 19:29:32 -0700

    Color support for cli output

--- src/e2c/run_script.py
+++ src/e2c/run_script.py
@@ -1,9 +1,24 @@
 """Functions for seaching in command line."""
 import click
+from colorama import Fore, Style
+from colorama import init as _init_colorama
 
 from e2c.db_connector import DBConnector
 
-_divider = "=" * 50
+
+_init_colorama(autoreset=True)
+
+_divider = Fore.WHITE + "-" * 4
+
+
+def _tab_echo(s, tabs=4):
+    """Echo via click with tabs at the beginning.
+
+    Args:
+        s (str): The string.
+        tabs (int, optional): Tabs before the string. Defaults to 4.
+    """
+    click.echo(tabs * " " + s)
 
 
 @click.group()
@@ -21,12 +36,26 @@ def search(word):
             "a lot" or "mirror".
     """
     res = DBConnector().query(word)
-    click.echo(_divider)
+    _tab_echo(_divider)
     if res:
-        click.echo(word + "\n" + res["definition"] + "\n\n" + res["trans"])
+        _tab_echo(Fore.CYAN + Style.BRIGHT + word + "\n")
+        for k in res:
+            if k in ("definition", "trans"):
+                items = res[k].split("\n")
+                _tab_echo(str(k) + ": ")
+                for item in items:
+                    _tab_echo("- " + item, tabs=8)
+            elif k in ("phonetic", "collins", "oxford", "bnc", "frq"):
+                _tab_echo(str(k) + ": " + str(res[k]))
     else:
-        click.echo("'" + word + "' can not be found in the database!")
-    click.echo(_divider)
+        _tab_echo(
+            Fore.RED
+            + Style.BRIGHT
+            + word
+            + Style.RESET_ALL
+            + " can not be found in the database!"
+        )
+    _tab_echo(_divider)
 
 
 cli.add_command(search)
