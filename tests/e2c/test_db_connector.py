75ad4f3218f16905f414fa90f42e6cda896a9efc
edxu96 <edxu96@outlook.com>
2020-07-10 16:24:40 +0200

    test error raise caused by wrong path in query

--- tests/e2c/test_db_connector.py
+++ tests/e2c/test_db_connector.py
@@ -10,10 +10,18 @@ def test_no_file_connect():
         _ = DBConnector("./no_such_file")
 
 
+def test_no_file_connect_word_query():
+    """Test database connection when .db file doesn't exist."""
+    with pytest.raises(ValueError):
+        _ = DBConnector("./no_such_file").query("play")
+
+
 def test_word_query():
     """Test single word query."""
     res = DBConnector().query("play")
-    assert isinstance(res, dict) and len(res) > 0
+    assert (
+        isinstance(res, dict) and len(res) == 13 and isinstance(res["id"], int)
+    )
 
 
 def test_non_exist_word_search():
