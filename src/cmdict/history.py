ddf7039d265afcf27a6ed2649d755d3c0bd589d0
Edward Xu <edxu96@outlook.com>
2023-06-08 22:15:15 +0200

    Able to run as main script. Add integration test (#140)

--- src/cmdict/history.py
+++ src/cmdict/history.py
@@ -1,6 +1,7 @@
-"""Functions for recording and reading user history."""
+"""Function to record and read user history."""
 import os
 import pathlib
+from typing import Optional
 
 from loguru import logger
 import yaml
@@ -10,7 +11,7 @@ _path = os.path.join(
 )
 
 
-def record(word, path=_path):
+def record(word: str, path: Optional[str] = _path):
     """Append queried word in a yaml file in lowercase format.
 
     If the path is directed to yaml file, there will be three
@@ -19,28 +20,29 @@ def record(word, path=_path):
     file does exist, we read its content first.
 
     Args:
-        word (str): to be appended at the end of the yaml file.
-        path (str): to the yaml file.
+        word: to be appended at the end of the yaml file.
+        path: to the yaml file.
 
     Raises:
         ValueError: when the path is not to a yaml file.
     """
     if not path.endswith(".yaml"):
-        raise ValueError(f"{path} is not a path to yaml file.")
+        raise ValueError(f'"{path}" is not a yaml file.')
 
     if not pathlib.Path(path).is_file():
         with open(path, "w") as f:
             yaml.safe_dump([word], f)
     else:
+        history: list[str] = None
         with open(path, "r") as f:
             try:
-                hist = yaml.safe_load(f)
+                history = yaml.safe_load(f)
             except yaml.YAMLError as exc:
                 logger.exception(exc)
 
-        if hist is None:
+        if history is None:
             with open(path, "w") as f:
                 yaml.safe_dump([word], f)
-        elif word not in hist:
+        elif word not in history:
             with open(path, "a") as f:
                 yaml.safe_dump([word], f)
